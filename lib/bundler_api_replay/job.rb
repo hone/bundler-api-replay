22f272131d66f9a376e53725888f50b4f645e670
Terence Lee <hone02@gmail.com>
2012-11-04 02:48:46 -0700

    remove check for http/https port

--- lib/bundler_api_replay/job.rb
+++ lib/bundler_api_replay/job.rb
@@ -3,14 +3,10 @@ require 'uri'
 require_relative '../bundler_api_replay'
 
 class BundlerApiReplay::Job
-  def initialize(request, host, port = nil)
+  def initialize(request, host, port = 80)
     @request = URI.parse("http://#{request}")
     @host    = host
     @port    = port
-
-    if @port.nil?
-      @request.scheme == "https" ? @port = 443 : @port = 80
-    end
   end
 
   def run
