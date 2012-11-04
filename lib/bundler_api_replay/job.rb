78776acfe5a0f2dcafce2e58c39146a57912e008
Terence Lee <hone02@gmail.com>
2012-11-04 01:50:14 -0600

    detect default port based on scheme

--- lib/bundler_api_replay/job.rb
+++ lib/bundler_api_replay/job.rb
@@ -3,15 +3,18 @@ require 'uri'
 require_relative '../bundler_api_replay'
 
 class BundlerApiReplay::Job
-  def initialize(request, host, port = 80)
-    @request = request
+  def initialize(request, host, port = nil)
+    @request = URI.parse(request)
     @host    = host
     @port    = port
+
+    if @port.nil?
+      @request.scheme == "https" ? @port = 443 : @port = 80
+    end
   end
 
   def run
-    uri  = URI.parse(@request)
     http = Net::HTTP.new(@host, @port)
-    http.request(Net::HTTP::Get.new("#{uri.path}#{uri.query}"))
+    http.request(Net::HTTP::Get.new("#{@request.path}#{@request.query}"))
   end
 end
