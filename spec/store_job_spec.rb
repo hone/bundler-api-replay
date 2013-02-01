dd8332663518658354d4bcdc0ce8b6b02d609daf
Terence Lee <hone02@gmail.com>
2013-02-01 11:56:18 -0800

    specs for StoreJob

---
+++ spec/store_job_spec.rb
@@ -0,0 +1,15 @@
+require 'sequel'
+require_relative 'spec_helper'
+require_relative '../lib/bundler_api_replay/store_job'
+
+describe BundlerApiReplay::StoreJob do
+  let(:conn) { Sequel.connect(ENV["TEST_DATABASE_URL"]) }
+  let(:path) { "/foo" }
+  let(:host) { 'localhost' }
+
+  it "runs the job" do
+    job    = BundlerApiReplay::StoreJob.new(conn, path, host)
+
+    expect { result }.not_to raise_error(Sequel::DatabaseError)
+  end
+end
