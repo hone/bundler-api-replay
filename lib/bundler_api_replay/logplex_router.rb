2f9a8213015b767a0c7024cc12c39041f5bb6550
Terence Lee <hone02@gmail.com>
2013-02-01 05:45:31 -0800

    Revert "update LogplexRouter for new logging format"
    This reverts commit 2efc2fbb8f1db0394db3e56d92b923377d6d6c71.

--- lib/bundler_api_replay/logplex_router.rb
+++ lib/bundler_api_replay/logplex_router.rb
@@ -3,21 +3,18 @@ require_relative 'logplex_processor'
 class BundlerApiReplay::LogplexRouter < BundlerApiReplay::LogplexProcessor
   RouterRegex = %r{
     (?<method>GET|POST){0}
-    (?<path>.*){0}
-    (?<host>.*){0}
-    (?<fwd>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}){0}
+    (?<request>.*){0}
     (?<dyno>[a-zA-Z]+[.][0-9]+){0}
     (?<queue>\d+){0}
     (?<wait>\d+){0}
-    (?<connect>\d+){0}
     (?<service>\d+){0}
     (?<status>\d+){0}
     (?<bytes>\d+){0}
 
-    ^at=info\smethod=\g<method>\spath=\g<path>\shost=\g<host>\sfwd=\g<fwd>\sdyno=\g<dyno>\squeue=\g<queue>\swait=\g<wait>ms\sconnect=\g<connect>ms\sservice=\g<service>ms\sstatus=\g<status>\sbytes=\g<bytes>$
+    ^\g<method>\s\g<request>\sdyno=\g<dyno>\squeue=\g<queue>\swait=\g<wait>ms\sservice=\g<service>ms\sstatus=\g<status>\sbytes=\g<bytes>$
   }x
 
-  attr_reader :method, :path, :host, :fwd, :dyno, :queue, :wait, :connect, :service, :status, :bytes
+  attr_reader :method, :request, :dyno, :queue, :wait, :service, :status, :bytes
 
   def initialize(input)
     super(input)
@@ -26,13 +23,10 @@ class BundlerApiReplay::LogplexRouter < BundlerApiReplay::LogplexProcessor
     if md
       @match   = true
       @method  = md[:method]
-      @path    = md[:path]
-      @host    = md[:host]
-      @fwd     = md[:fwd]
+      @request = md[:request]
       @dyno    = md[:dyno]
       @queue   = md[:queue]
       @wait    = md[:wait]
-      @connect = md[:connect]
       @service = md[:service]
       @status  = md[:status]
       @bytes   = md[:bytes]
