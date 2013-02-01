47892dcd14cf6eb3641b154287d6ed357c68f6bd
Terence Lee <hone02@gmail.com>
2013-02-01 05:47:37 -0800

    Revert "LogplexProcessor for new log format"
    This reverts commit 5eb2df963e3a73ade7807aa481f18e564b4fdc9f.
    
    Conflicts:
    	lib/bundler_api_replay/logplex_processor.rb

--- lib/bundler_api_replay/logplex_processor.rb
+++ lib/bundler_api_replay/logplex_processor.rb
@@ -6,20 +6,21 @@ class BundlerApiReplay::LogplexProcessor
   LineRegex = %r{
     (?<time>\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\+00:00){0}
     (?<process>[a-z0-9\-\_\.]+){0}
+    (?<uuid>[a-z0-9\-\_\.]+){0}
     (?<body>.*){0}
 
-    ^\g<time>\s(heroku|app)\[\g<process>\]:\s\g<body>$
+    ^\d+\s\<\d+\>1\s\g<time>\s[a-z0-9-]+\s\g<process>\s\g<uuid>\s\-\s\g<body>$
   }x
 
-  attr_reader :time, :process, :body
+  attr_reader :time, :process, :uuid, :body
 
   def initialize(input)
     @input     = input
     match_data = LineRegex.match(input)
-
     if match_data
       @time    = Time.parse(match_data[:time])
       @process = match_data[:process]
+      @uuid    = match_data[:uuid]
       @body    = match_data[:body]
     else
       raise BundlerApiReplay::LogParseError.new("Could not parse: #{input}")
