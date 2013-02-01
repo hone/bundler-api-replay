a098cf61f82e1c53b7aa9a992477607bddb528ad
Terence Lee <hone02@gmail.com>
2013-02-01 12:36:41 -0800

    be able to read path, host, port on StoreJob

--- lib/bundler_api_replay/store_job.rb
+++ lib/bundler_api_replay/store_job.rb
@@ -1,6 +1,8 @@
 require_relative '../bundler_api_replay'
 
 class BundlerApiReplay::StoreJob
+  attr_reader :path, :host, :port
+
   def initialize(conn, path, host, port = 80)
     @conn = conn
     @path = path
