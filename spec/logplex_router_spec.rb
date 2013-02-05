19c2ba93616f07ea94b0c31aa3cfe4ceb719736f
Terence Lee <hone02@gmail.com>
2013-02-05 02:36:45 +0100

    Revert "Revert "update LogplexRouter for new logging format""
    This reverts commit 2f9a8213015b767a0c7024cc12c39041f5bb6550.

--- spec/logplex_router_spec.rb
+++ spec/logplex_router_spec.rb
@@ -5,7 +5,7 @@ describe BundlerApiReplay::LogplexRouter do
   let(:processor) { BundlerApiReplay::LogplexRouter.new(input) }
   
   context "when using a router logplex line" do
-    let(:input) { "339 <158>1 2012-11-02T20:51:42+00:00 heroku router d.030f3924-0da0-4be1-8a33-6b476c52e3ea - GET bundler-api.herokuapp.com/api/v1/dependencies?gems=SexpProcessor,oauth2,roauth,ruby_core_source,simple_oauth,windows-api,win32-api,bones-rcov,bones-rubyforge,bones-rspec,bones-zentest dyno=web.5 queue=0 wait=0ms service=27ms status=200 bytes=8518" }
+    let(:input) { "2012-12-22T04:41:31+00:00 heroku[router]: at=info method=GET path=/api/v1/dependencies?gems=bones-rcov,bones-rubyforge,bones-rspec,bones-zentest,git,metaclass,net-ssh,SexpProcessor,test-unit host=bundler-api.herokuapp.com fwd=72.4.120.81 dyno=web.4 queue=0 wait=0ms connect=2ms service=13ms status=200 bytes=7118" }
 
     it "creates a valid object" do
       expect(processor).to be_true
@@ -19,12 +19,20 @@ describe BundlerApiReplay::LogplexRouter do
       expect(processor.method).to eq("GET")
     end
 
-    it "parses the request" do
-      expect(processor.request).to eq("bundler-api.herokuapp.com/api/v1/dependencies?gems=SexpProcessor,oauth2,roauth,ruby_core_source,simple_oauth,windows-api,win32-api,bones-rcov,bones-rubyforge,bones-rspec,bones-zentest")
+    it "parses the path" do
+      expect(processor.path).to eq("/api/v1/dependencies?gems=bones-rcov,bones-rubyforge,bones-rspec,bones-zentest,git,metaclass,net-ssh,SexpProcessor,test-unit")
+    end
+
+    it "parses the host" do
+      expect(processor.host).to eq("bundler-api.herokuapp.com")
+    end
+
+    it "parses the fwd" do
+      expect(processor.fwd).to eq("72.4.120.81")
     end
 
     it "parses the dyno" do
-      expect(processor.dyno).to eq("web.5")
+      expect(processor.dyno).to eq("web.4")
     end
 
     it "parses the queue" do
@@ -35,8 +43,12 @@ describe BundlerApiReplay::LogplexRouter do
       expect(processor.wait).to eq("0")
     end
 
+    it "parses the connect" do
+      expect(processor.connect).to eq("2")
+    end
+
     it "parses the service" do
-      expect(processor.service).to eq("27")
+      expect(processor.service).to eq("13")
     end
 
     it "parses the status" do
@@ -44,12 +56,12 @@ describe BundlerApiReplay::LogplexRouter do
     end
 
     it "parses the bytes" do
-      expect(processor.bytes).to eq("8518")
+      expect(processor.bytes).to eq("7118")
     end
   end
 
   context "when using a non router logplex line" do
-    let(:input) { %q{348 <13>1 2012-11-04T08:37:34+00:00 app web.2 d.030f3924-0da0-4be1-8a33-6b476c52e3ea - 72.4.120.81 - - [04/Nov/2012 08:37:34] "GET /api/v1/dependencies?gems=rails,devise,sqlite3,sass-rails,coffee-rails,uglifier,jquery-rails,rails-backbone,powder,awesome_print,annotate,jasmine-headless-webkit,jasmine,jasminerice,rspec-rails HTTP/1.1" 200 93127 0.1165"} }
+    let(:input) { %q{2012-12-22T04:45:19+00:00 app[web.9]: 72.4.120.81 - - [22/Dec/2012 04:45:19] "GET /api/v1/dependencies.json?gems=sso-auth HTTP/1.1" 200 1073 0.0078} }
 
     it "creates a valid object" do
       expect(processor).to be_true
