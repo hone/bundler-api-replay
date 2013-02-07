0f2ef80ff12f240175e4ab3ee5ddc8f03b764f17
Larry Marburger <larry@marburger.cc>
2013-02-07 15:27:13 -0500

    Log and ignore EOFErrors when replaying

--- lib/bundler_api_replay/job.rb
+++ lib/bundler_api_replay/job.rb
@@ -13,9 +13,17 @@ class BundlerApiReplay::Job
   def run
     http = Net::HTTP.new(@host, @port)
     http.request(Net::HTTP::Get.new(@path))
+  rescue EOFError => e
+    # Bad responses from crashed hosts raise an EOFError.
+    #   https://gist.github.com/lmarburger/9d063165e57743987a92
+    logger.warn "EOFError: #{to_s}"
   end
 
   def to_s
     "BundlerApiReplay::Job: path: #{@path}, port: #{@port}, host: #{@host}"
+  end
+
+  def logger(io = STDOUT)
+    @logger ||= Logger.new(io)
   end
 end
